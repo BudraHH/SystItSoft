9ca5c6ddfa7ca889fc93ea353b58a14ea36faff3
BudraHH <hariharabudra@gmail.com>
2025-02-04 12:20:46 +0530

    mm

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sysitsoft/",
+  base: "https://github.com/BudraHH/SystItSoft/",
   server: {
     middlewareMode: false,
   },
